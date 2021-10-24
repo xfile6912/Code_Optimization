255e005440ce99dd0fffec314fcb7aee0fd3470c
xfile6912 <xfile6912@naver.com>
2021-10-25 08:58:34 +0900

    implement cpu_thread parts

--- CPU_thread/sum_global_array.cpp
+++ CPU_thread/sum_global_array.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <thread>
-#include <mutex>
 #include <chrono>//시간을 측정하기위해 사용
 
 using namespace std;
@@ -11,12 +10,13 @@ using namespace std::chrono;
 /* 0부터 n-1까지의 sum을 구하는 작업을 진행하게 됨 */
 
 
-void sum_global_variable(void* vargp);// thread가 실행할 루틴
+void sum_global_array(void* vargp);// thread가 실행할 루틴
+void total_sum(int thread_num);
 
 //공유되는 변수
 long long global_sum = 0;  //전체 sum을 저장하는데 사용되는 변수
 long num_per_thread; //thread당 맡은 수의 개수
-mutex lock_mutex;
+long long global_array[MAX_THREADS];//각 thread가 자신이 맡은 영역에 대한 합을 저장하는 배열
 long long N = (1 << 30);
 
 int main(int argc, char** argv)
@@ -26,25 +26,29 @@ int main(int argc, char** argv)
     thread threads[MAX_THREADS]; //thread를 저장하는데 사용되는 변수
     long thread_num;
 
-    
+
     //thread의 개수를 바꾸어 가면서 test
-    for (thread_num = 1; thread_num <= MAX_THREADS; thread_num *= 2)
+    for (thread_num=1; thread_num <= MAX_THREADS; thread_num *= 2)
     {
+
         //필요한 정보들 초기화
         global_sum = 0;
+        memset(global_array, 0, sizeof(global_array));
         num_per_thread = N / thread_num;
-
         auto start = high_resolution_clock::now();
 
         for (i = 0; i < thread_num; i++) {
             id[i] = i;
-            threads[i] = thread(sum_global_variable, &id[i]);
+            threads[i] = thread(sum_global_array, &id[i]);
         }
 
         for (i = 0; i < thread_num; i++)
         {
             threads[i].join();
         }
+
+        //최종적으로 global_array에 있는 내용들을 취합해서 global_sum에 저장해주는 과정 수행
+        total_sum(thread_num);
 
         auto end = high_resolution_clock::now();
 
@@ -58,13 +62,23 @@ int main(int argc, char** argv)
         {
             printf("Result is correct\n");
         }
-
         printf("걸린시간 : %d ms\n\n", duration_cast<milliseconds>(end - start).count());
+
     }
     exit(0);
 }
+
+
+//메인 thread에서 최종적으로 global_array의 내용들을 취합해서 sum을 하는 과정
+void total_sum(int thread_num)
+{
+    long i;
+    for (i = 0; i < thread_num; i++)
+        global_sum += global_array[i];
+}
+
 //각 thread가 실행할 함수
-void sum_global_variable(void* vargp)
+void sum_global_array(void* vargp)
 {
     long id = (*(long*)vargp);
     long start = id * num_per_thread;
@@ -75,8 +89,6 @@ void sum_global_variable(void* vargp)
     long i;
     for (i = start; i < end; i++)
     {
-        lock_mutex.lock();
-        global_sum += i;//global_sum이라는 공유데이터를 변경하는 코드영역, 즉 크리티컬섹션을 semaphore로 감싸줌
-        lock_mutex.unlock();
+        global_array[id] += i;//global_array의 자신이 담당하는 index에, 자신이 맡은 영역의 sum을 저장해줌
     }
 }
--- CPU_thread/sum_local_register.cpp
+++ CPU_thread/sum_local_register.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <thread>
-#include <mutex>
 #include <chrono>//시간을 측정하기위해 사용
 
 using namespace std;
@@ -11,12 +10,13 @@ using namespace std::chrono;
 /* 0부터 n-1까지의 sum을 구하는 작업을 진행하게 됨 */
 
 
-void sum_global_variable(void* vargp);// thread가 실행할 루틴
+void sum_local_register(void* vargp);// thread가 실행할 루틴
+void total_sum(int thread_num);
 
 //공유되는 변수
 long long global_sum = 0;  //전체 sum을 저장하는데 사용되는 변수
 long num_per_thread; //thread당 맡은 수의 개수
-mutex lock_mutex;
+long long global_array[MAX_THREADS];//각 thread가 자신이 맡은 영역에 대한 합을 저장하는 배열
 long long N = (1 << 30);
 
 int main(int argc, char** argv)
@@ -26,25 +26,29 @@ int main(int argc, char** argv)
     thread threads[MAX_THREADS]; //thread를 저장하는데 사용되는 변수
     long thread_num;
 
-    
+
     //thread의 개수를 바꾸어 가면서 test
     for (thread_num = 1; thread_num <= MAX_THREADS; thread_num *= 2)
     {
+
         //필요한 정보들 초기화
         global_sum = 0;
+        memset(global_array, 0, sizeof(global_array));
         num_per_thread = N / thread_num;
-
         auto start = high_resolution_clock::now();
 
         for (i = 0; i < thread_num; i++) {
             id[i] = i;
-            threads[i] = thread(sum_global_variable, &id[i]);
+            threads[i] = thread(sum_local_register, &id[i]);
         }
 
         for (i = 0; i < thread_num; i++)
         {
             threads[i].join();
         }
+
+        //최종적으로 global_array에 있는 내용들을 취합해서 global_sum에 저장해주는 과정 수행
+        total_sum(thread_num);
 
         auto end = high_resolution_clock::now();
 
@@ -58,25 +62,42 @@ int main(int argc, char** argv)
         {
             printf("Result is correct\n");
         }
-
         printf("걸린시간 : %d ms\n\n", duration_cast<milliseconds>(end - start).count());
+
     }
     exit(0);
 }
+
+
+//메인 thread에서 최종적으로 global_array의 내용들을 취합해서 sum을 하는 과정
+void total_sum(int thread_num)
+{
+    long i;
+    for (i = 0; i < thread_num; i++)
+        global_sum += global_array[i];
+}
+
 //각 thread가 실행할 함수
-void sum_global_variable(void* vargp)
+void sum_local_register(void* vargp)
 {
     long id = (*(long*)vargp);
     long start = id * num_per_thread;
     long end = start + num_per_thread;
+    //thread에 local변수를 만들어서 이를 이용하도록 하여
+    //메인 메모리에 대한 접근을 줄이고
+    //(컴파일러가 최적화 하면서 local변수는 register를 이용하도록 하기 때문에)
+    //메인 메모리 영역에 있는 global_array는 
+    //한번만 접근하게 해줌으로써 속도를 높임
+    long long local_sum = 0;
     if (end > N)
         end = N;
 
     long i;
     for (i = start; i < end; i++)
     {
-        lock_mutex.lock();
-        global_sum += i;//global_sum이라는 공유데이터를 변경하는 코드영역, 즉 크리티컬섹션을 semaphore로 감싸줌
-        lock_mutex.unlock();
+        local_sum += i;//local_sum을 이용해 자신이 맡은 영역의 sum을 계산해주도록 함
     }
+
+    //global array는 각 thread마다 한번만 접근함
+    global_array[id] = local_sum;
 }
